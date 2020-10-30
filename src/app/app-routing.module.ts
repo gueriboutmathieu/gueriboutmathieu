fbe93140099ddca9c59dbdbdfeb7f5f622052b14
Meridolian <mgueribout@gmail.com>
2020-10-30 19:48:00 +0100

    adding all useful components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AchievementsComponent } from './components/achievements/achievements.component';
+import { BioComponent } from './components/bio/bio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EducationComponent } from './components/education/education.component';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
+import { HomeComponent } from './components/home/home.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  },
+  {
+    path: "/home",
+    component: HomeComponent
+  },
+  {
+    path: "/bio",
+    component: BioComponent
+  },
+  {
+    path: "/skills",
+    component: SkillsComponent
+  },
+  {
+    path: "/work-experience",
+    component: WorkExperienceComponent
+  }, 
+  {
+    path: "/education",
+    component: EducationComponent
+  },
+  {
+    path: "/achievements",
+    component: AchievementsComponent
+  },
+  {
+    path: "/hobbies",
+    component: HobbiesComponent
+  },
+  {
+    path: "/contact",
+    component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BioComponent } from './components/bio/bio.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { AchievementsComponent } from './components/achievements/achievements.component';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GalaxyComponent } from './components/galaxy/galaxy.component';
+import { LanguageComponent } from './components/language/language.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BioComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    AchievementsComponent,
+    HobbiesComponent,
+    ContactComponent,
+    NavbarComponent,
+    GalaxyComponent,
+    LanguageComponent
   ],
   imports: [
     BrowserModule,
